5ff88090ba7de70afc37135705f6dbc2d56f5107
Rene Milk <rene.milk@uni-muenster.de>
2012-06-19 17:52:38 +0200

    reformat msfem.cc

---
+++ src/msfem_globals.hh
@@ -0,0 +1,50 @@
+#ifndef MSFEM_GLOBALS_HH
+#define MSFEM_GLOBALS_HH
+
+//! ---------------------- important variables ---------------------------------
+enum { dimension = GridType :: dimension};
+//name of the error file in which the data will be saved
+std :: string filename_;
+std :: string path_;
+int total_refinement_level_;
+int coarse_grid_level_;
+// only for uniform computations / use the same number of layers for every coarse grid entity
+int number_of_layers_;
+//! -----------------------------------------------------------------------------
+
+
+
+//! ---------------------- local error indicators --------------------------------
+// ----- local error indicators (for each coarse grid element T) -------------
+int max_loop_number = 10;
+bool local_indicators_available_ = false;
+// local coarse residual, i.e. H ||f||_{L^2(T)}
+std :: vector < std :: vector < RangeType > > loc_coarse_residual_( max_loop_number );
+// local coarse grid jumps (contribute to the total coarse residual)
+std :: vector < std :: vector < RangeType > > loc_coarse_grid_jumps_( max_loop_number );
+// local projection error (we project to get a globaly continous approximation)
+std :: vector < std :: vector < RangeType > > loc_projection_error_( max_loop_number );
+// local jump in the conservative flux
+std :: vector < std :: vector < RangeType > > loc_conservative_flux_jumps_( max_loop_number );
+// local approximation error
+std :: vector < std :: vector < RangeType > > loc_approximation_error_( max_loop_number );
+// local sum over the fine grid jumps (for a fixed subgrid that cooresponds with a coarse entity T)
+std :: vector < std :: vector < RangeType > > loc_fine_grid_jumps_( max_loop_number );
+
+
+std :: vector < RangeType > total_coarse_residual_( max_loop_number );
+std :: vector < RangeType > total_projection_error_( max_loop_number );
+std :: vector < RangeType > total_coarse_grid_jumps_( max_loop_number );
+std :: vector < RangeType > total_conservative_flux_jumps_( max_loop_number );
+std :: vector < RangeType > total_approximation_error_( max_loop_number );
+std :: vector < RangeType > total_fine_grid_jumps_( max_loop_number );
+std :: vector < RangeType > total_estimated_H1_error_( max_loop_number );
+
+bool repeat_algorithm_ = true;
+int loop_number_ = 0;
+
+#ifdef ADAPTIVE
+double error_tolerance_;
+#endif
+
+#endif // MSFEM_GLOBALS_HH
---
+++ src/outputparameter.hh
@@ -0,0 +1,56 @@
+#ifndef OUTPUTPARAMETER_HH
+#define OUTPUTPARAMETER_HH
+
+#include <string>
+
+// define output traits
+struct myDataOutputParameters : public DataOutputParameters {
+
+public:
+    std::string my_prefix_;
+    std::string my_path_;
+
+    void set_prefix( std::string my_prefix )
+    {
+        my_prefix_ = my_prefix;
+        // std :: cout << "Set prefix. my_prefix_ = " << my_prefix_ << std :: endl;
+    }
+
+    void set_path( std::string my_path )
+    {
+        my_path_ = my_path;
+    }
+
+    // base of file name for data file
+    std::string prefix() const
+    {
+        if (my_prefix_ == "")
+            return "solutions";
+        else
+            return my_prefix_;
+    }
+
+    // path where the data is stored
+    std::string path() const
+    {
+        if (my_path_ == "")
+            return "data_output_msfem";
+        else
+            return my_path_;
+
+    }
+
+
+    // format of output:
+    int outputformat() const
+    {
+        //return 0; // GRAPE (lossless format)
+        return 1;   // VTK
+        //return 2; // VTK vertex data
+        //return 3; // gnuplot
+    }
+
+
+};
+
+#endif // OUTPUTPARAMETER_HH
