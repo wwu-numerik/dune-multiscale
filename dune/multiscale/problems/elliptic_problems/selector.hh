d20ee77c7b9eab1f6f41af21e4eeeb3dcdd083e5
Rene Milk <rene.milk@uni-muenster.de>
2012-06-29 13:58:59 +0200

    always include all problem headers in selector
    enables syntax check at least

--- dune/multiscale/problems/elliptic_problems/selector.hh
+++ dune/multiscale/problems/elliptic_problems/selector.hh
@@ -1,7 +1,18 @@
 #ifndef DUNE_MS_PROBLEMS_SELECTOR_HH
 #define DUNE_MS_PROBLEMS_SELECTOR_HH
 
+//for i in $(ls *hh) ; do echo \#include \"${i}\" ; done
+#include "easy.hh"
+#include "eight.hh"
+#include "five.hh"
+#include "four.hh"
+#include "nine.hh"
 #include "one.hh"
+#include "seven.hh"
+#include "six.hh"
+#include "ten.hh"
+#include "three.hh"
+#include "toy.hh"
 #include "two.hh"
 
 namespace Problem {
