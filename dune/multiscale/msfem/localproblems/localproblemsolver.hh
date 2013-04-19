87a2afe72d1d10fc97041b33504a612a64198718
Sven Kaulmann <sven.kaulmann@mathematik.uni-stuttgart.de>
2013-04-19 18:17:37 +0200

    Had to temporarily disable clement oversampling because lagrangeInterpolation is not available any more

--- dune/multiscale/msfem/localproblems/localproblemsolver.hh
+++ dune/multiscale/msfem/localproblems/localproblemsolver.hh
@@ -374,7 +374,7 @@ public:
       if ( clement )
       {
 
-        HostDiscreteFunctionType zero("zero", specifier_.coarseSpace());
+/*        HostDiscreteFunctionType zero("zero", specifier_.coarseSpace());
         zero.clear();
         const double dummy = 12345.67890;
         double solverEps = 1e-8 ;
@@ -403,8 +403,8 @@ public:
         {
           DSC::Profiler::ScopedTiming st("uzawa");
           InverseUzawaOperatorType uzawa( locprob_fem_biCGStab, clement_interpolation_op, dummy, solverEps, maxIterations, true);
-          uzawa( local_problem_rhs, zero /*interpolation is zero*/, local_problem_solution, lagrange_multiplier );
-        }
+          uzawa( local_problem_rhs, zero *//*interpolation is zero*//*, local_problem_solution, lagrange_multiplier );
+        }*/
       }
       else
       { locprob_fem_biCGStab(local_problem_rhs, local_problem_solution); }
