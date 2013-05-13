8d195750a0aa33a0be370e4d3143fbc9341541e0
Sven Kaulmann <sven.kaulmann@mathematik.uni-stuttgart.de>
2013-05-13 11:30:38 +0200

    Added missing file

---
+++ common.mk
@@ -0,0 +1,13 @@
+GRIDTYPE=ALBERTAGRID
+GRIDDIM=2
+PROBLEM_NAMES= Toy One Two Three Four Five Six Seven Eight Nine Ten
+PROBLEM=Nine
+	
+CPPFLAGS = $(AM_CPPFLAGS) -DGRIDDIM=$(GRIDDIM) -D$(GRIDTYPE) -DWORLDDIM=$(GRIDDIM) -DPROBLEM_NAME=$(PROBLEM)
+
+# The libraries have to be given in reverse order (most basic libraries
+# last).  Also, due to some misunderstanding, a lot of libraries include the
+# -L option in LDFLAGS instead of LIBS -- so we have to include the LDFLAGS
+# here as well.
+LDFLAGS = $(AM_LDFLAGS)
+LDADD = $(AM_LDADD)
