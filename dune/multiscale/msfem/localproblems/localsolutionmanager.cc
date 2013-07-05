6749f9629d1538ae65b7f4df88828bd483a2a963
Sven Kaulmann <sven.kaulmann@mathematik.uni-stuttgart.de>
2013-07-05 09:17:21 +0200

    Added local solution manager (this time with file content and build system)

---
+++ dune/multiscale/msfem/localproblems/localsolutionmanager.cc
@@ -0,0 +1,86 @@
+#ifdef HAVE_CMAKE_CONFIG#ifdef HAVE_CMAKE_CONFIG
+  #include "cmake_config.h"
+#elif defined (HAVE_CONFIG_H)
+  #include <config.h>
+#endif // ifdef HAVE_CMAKE_CONFIG
+
+
+// - Dune includes
+#include <dune/multiscale/common/traits.hh>
+// #include <dune/multiscale/msfem/localproblems/subgrid-list.hh>
+// #include <dune/stuff/fem/functions/checks.hh>
+#include <dune/multiscale/msfem/msfem_traits.hh>
+#include <dune/multiscale/tools/discretefunctionwriter.hh>
+
+#include <dune/multiscale/msfem/msfem_traits.hh>
+#include <dune/multiscale/tools/discretefunctionwriter.hh>
+#include <dune/stuff/common/memory.hh>
+
+#include "localsolutionmanager.hh"
+
+namespace Dune {
+namespace Multiscale {
+namespace MsFEM {
+LocalSolutionManager::LocalSolutionManager(const CoarseEntityType& coarseEntity, SubGridListType& subgridList,
+                                           const MacroMicroGridSpecifierType& gridSpecifier)
+  : subgridList_(subgridList),
+    gridSpecifier_(gridSpecifier),
+    subGridPart_(subgridList_.getSubGrid(coarseEntity)),
+    localDiscreteFunctionSpace_(subGridPart_),
+    coarseId_(gridSpecifier_.coarseSpace().gridPart().grid().globalIdSet().id(coarseEntity)),
+    loaded_(false),
+    numLocalProblems_((DSC_CONFIG_GET("msfem.oldLocalProblemSolve", false)) ?
+                      GridSelector::dimgrid : localDiscreteFunctionSpace_.mapper().maxNumDofs()),
+    localSolutions_(numLocalProblems_),
+    localSolutionLocation_((boost::format("local_problems/_localProblemSolutions_%d")
+                            % coarseId_).str())
+{
+  for (auto& it : localSolutions_)
+    it = DSC::make_unique< DiscreteFunctionType >("Local problem Solution", localDiscreteFunctionSpace_);
+
+}
+
+LocalSolutionManager::LocalSolutionVectorType& LocalSolutionManager::getLocalSolutions()
+{
+  return localSolutions_;
+}
+
+const LocalSolutionManager::DiscreteFunctionSpaceType& LocalSolutionManager::getLocalDiscreteFunctionSpace() const
+{
+  return localDiscreteFunctionSpace_;
+}
+
+const LocalSolutionManager::SubGridPartType& LocalSolutionManager::getSubGridPart() const
+{
+  return subGridPart_;
+}
+
+void LocalSolutionManager::loadLocalSolutions()
+{
+  // reader for the cell problem data file:
+  DiscreteFunctionReader reader(localSolutionLocation_);
+
+  for (unsigned int i = 0; i < numLocalProblems_; ++i) {
+    localSolutions_[i]->clear();
+    reader.read(i, *(localSolutions_[i]));
+  }
+  loaded_ = true;
+  return;
+} // loadLocalSolutions
+
+void LocalSolutionManager::saveLocalSolutions() const
+{
+  // reader for the cell problem data file:
+  DiscreteFunctionWriter writer(localSolutionLocation_);
+
+  for (auto& it : localSolutions_)
+    writer.append(*it);
+} // saveLocalSolutions
+
+bool LocalSolutionManager::solutionsWereLoaded() const
+{
+  return loaded_;
+}
+}
+}
+}
