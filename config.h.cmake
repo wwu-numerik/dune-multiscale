d41fa762b6bff192981f7320c55009f45e8b7462
Rene Milk <rene.milk@uni-muenster.de>
2013-07-19 17:25:07 +0200

    [cmake] basic dune-cmake stuffs

---
+++ config.h.cmake
@@ -0,0 +1,43 @@
+/* begin dune-multiscale */
+
+/* Define to the version of dune-multiscale */
+#define DUNE_MULTISCALE_VERSION "${DUNE_MULTISCALE_VERSION}"
+
+/* Define to the major version of dune-multiscale */
+#define DUNE_MULTISCALE_VERSION_MAJOR ${DUNE_MULTISCALE_VERSION_MAJOR}
+
+/* Define to the minor version of dune-multiscale */
+#define DUNE_MULTISCALE_VERSION_MINOR ${DUNE_MULTISCALE_VERSION_MINOR}
+
+/* Define to the revision of dune-multiscale */
+#define DUNE_MULTISCALE_VERSION_REVISION ${DUNE_MULTISCALE_VERSION_REVISION}
+
+#undef COMMIT
+
+#ifndef DUNE_MULTISCALE_CONFIG_H
+#define DUNE_MULTISCALE_CONFIG_H
+
+/* the git tag / commit we build from */
+#define COMMIT "@COMMIT@"
+
+#define ALBERTA_DIM WORLDDIM
+#define ALBERTA_DEBUG 1
+#cmakedefine @ENABLE_ALUGRID@ 
+#cmakedefine @ENABLE_ABERTA@
+#define ENABLE_MPI @ENABLE_MPI@
+
+#define DUNE_COMMON_FIELDVECTOR_SIZE_IS_METHOD 1
+
+#ifdef NDEBUG
+  #define DNDEBUG
+#endif
+
+//compiler quirks workarounds
+#ifdef __clang__
+  class type_info;
+  #define BOOST_HAS_RVALUE_REFS 1
+#endif
+
+#endif  /* DUNE_MULTISCALE_CONFIG_H */
+
+/* end dune-multiscale */
