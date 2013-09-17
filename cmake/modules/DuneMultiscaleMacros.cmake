057e033095e82f653d34d1763516047b978376c9
Rene Milk <rene.milk@uni-muenster.de>
2013-09-17 18:46:49 +0200

    [cmake] use same petsc test on all platforms

--- cmake/modules/DuneMultiscaleMacros.cmake
+++ cmake/modules/DuneMultiscaleMacros.cmake
@@ -1,7 +1,3 @@
 message(AUTHOR_WARNING "TODO: Implement module test.")
 
-if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-  find_package(PETSc COMPONENTS CXX REQUIRED)
-else(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-  find_package(PETSc REQUIRED)
-endif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+find_package(PETSc COMPONENTS CXX REQUIRED)
