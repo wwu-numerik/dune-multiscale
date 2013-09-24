c6f5e14c79e9b0b216f5018665e552adeb9809ea
Rene Milk <rene.milk@uni-muenster.de>
2013-09-24 17:30:18 +0200

    remove dead code in linear-lagrange-interpol

--- dune/multiscale/tools/misc/linear-lagrange-interpolation.hh
+++ dune/multiscale/tools/misc/linear-lagrange-interpolation.hh
@@ -87,58 +87,10 @@ public:
   // Constructor for LinearLagrangeFunction2D
   inline explicit LinearLagrangeFunction2D(DiscreteFunctionSpaceType& dfSpace, EntityPointerType& entity)
     : localFunc_(DSC::make_unique(dfSpace, *entity)) {}
-  /*: a_0_( entity->geometry().corner(0) )
-      , p_a_0_(0.0)
-      , a_1_( entity->geometry().corner(1) )
-      , p_a_1_(0.0)
-      , a_2_( entity->geometry().corner(2) )
-      , p_a_2_(0.0)
-      , entity_(&entity)
-  {}
-*/
+
   inline void evaluate(const DomainType& x, RangeType& y) const {
     assert(localFunc_);
     localFunc_->evaluate(x, y);
-
-    /*RangeType lambda_1;
-    RangeType lambda_0;
-    if (a_0_[0] == a_2_[0])
-    {
-      lambda_1 = (x[0] - a_2_[0]) / (a_1_[0] - a_2_[0]);
-
-      lambda_0 = ( (x[1] - a_2_[1]) - ( lambda_1 * (a_1_[1] - a_2_[1]) ) ) / (a_0_[1] - a_2_[1]);
-    } else {
-      lambda_1
-        = ( (x[1] - a_2_[1]) - ( (a_0_[1] - a_2_[1]) * ( (x[0] - a_2_[0]) / (a_0_[0] - a_2_[0]) ) ) )
-          / ( (a_1_[1] - a_2_[1]) - ( (a_0_[1] - a_2_[1]) * ( (a_1_[0] - a_2_[0]) / (a_0_[0] - a_2_[0]) ) ) );
-
-      lambda_0
-        = ( (x[0] - a_2_[0]) / (a_0_[0] - a_2_[0]) ) - ( ( (a_1_[0] - a_2_[0]) / (a_0_[0] - a_2_[0]) ) * lambda_1 );
-    }
-
-    y = (p_a_0_ * lambda_0) + (p_a_1_ * lambda_1) + ( p_a_2_ * (1.0 - lambda_0 - lambda_1) );
-
-    if (boost::math::isnan(lambda_1))
-    {
-      DSC_LOG_ERROR << "lambda_1 is nan! Details:" << std::endl << std::endl
-                    << "a_0_ = " << a_0_ << std::endl
-                    << "a_1_ = " << a_1_ << std::endl
-                    << "a_2_ = " << a_2_ << std::endl << std::endl
-                    << "p_a_0_ = " << p_a_0_ << std::endl
-                    << "p_a_1_ = " << p_a_1_ << std::endl
-                    << "p_a_2_ = " << p_a_2_ << std::endl << std::endl
-                    << "a_0_[0] - a_2_[0] = " << a_0_[0] - a_2_[0] << std::endl
-                    << "Whole denominator = "
-                    << ( (a_1_[1]
-                              - a_2_[1]) - ( (a_0_[1] - a_2_[1]) * ( (a_1_[0] - a_2_[0]) / (a_0_[0] - a_2_[0]) ) ) ) <<
-    std::endl;
-      DUNE_THROW(Dune::InvalidStateException,"NaN");
-    }
-
-    if (boost::math::isnan(lambda_0))
-    {
-      DUNE_THROW(Dune::InvalidStateException,"lambda_0 is nan");
-    }*/
   } // evaluate
 
   template <typename DiscreteFunctionType>
@@ -149,38 +101,12 @@ public:
     for (int i = 0; i < baseSetSize; ++i) {
       (*localFunc_)[i] = loc_func[i];
     }
-
-    /*const int number_of_nodes = ( *(*entity_) ).template count< 2 >();
-
-    if ( number_of_nodes != int( loc_func.basisFunctionSet().size() ) ) {
-      DSC_LOG_ERROR << "Error! Inconsistency in 'linear-lagrange-interpolation.hh'." << std::endl;
-    }
-
-    for (int i = 0; i < number_of_nodes; ++i) {
-      const NodePointerType node = ( *(*entity_) ).template subEntity< 2 >(i);
-
-      if ( node->geometry().corner(0) != (*entity_)->geometry().corner(i) ) {
-        DSC_LOG_ERROR << "Error! Inconsistency in 'linear-lagrange-interpolation.hh'." << std::endl;
-      }
-
-      if (i == 0)
-      { p_a_0_ = loc_func[0]; }
-
-      if (i == 1)
-      { p_a_1_ = loc_func[1]; }
-
-      if (i == 2)
-      { p_a_2_ = loc_func[2]; }
-    }
-    check_dofs();*/
   } // set_corners
 
 private:
   LocalFunctionType localFunc_;
 };
 
-//    void DUNE_DEPRECATED_MSG( "This Dune::Geometry is still a reference to its implementation." )
-//    deprecationWarning ( integral_constant< bool, true > ) {}
 
 //! DEPRECATED should be eventually replaced by the LinearLagrangeFunction2D implementation
 template <class FunctionSpaceImp>
