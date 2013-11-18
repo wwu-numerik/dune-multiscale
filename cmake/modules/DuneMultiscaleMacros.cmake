a745cdb5126dc3bbab48e4c790c11ff28c5c7676
Rene Milk <rene.milk@uni-muenster.de>
2013-11-18 18:59:47 +0100

    [cmake] move metis flags

--- cmake/modules/DuneMultiscaleMacros.cmake
+++ cmake/modules/DuneMultiscaleMacros.cmake
@@ -1,3 +1,8 @@
 message(AUTHOR_WARNING "TODO: Implement module test.")
 
 find_package(PETSc COMPONENTS CXX )
+
+find_package(METIS)
+find_package(ParMETIS)
+include(cmake/modules/AddMETISFlags.cmake)
+include(AddParMETISFlags)
