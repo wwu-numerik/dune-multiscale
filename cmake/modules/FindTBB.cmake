543ee649a88bfd9afae25b751c4123f190d313fa
Rene Milk <rene.milk@uni-muenster.de>
2013-11-22 15:52:10 +0100

    [cmake] adds tbb dependecy

---
+++ cmake/modules/FindTBB.cmake
@@ -0,0 +1,91 @@
+## ---------------------------------------------------------------------
+## $Id$
+##
+## Copyright (C) 2012 - 2013 by the deal.II authors
+##
+## This file is part of the deal.II library.
+##
+## The deal.II library is free software; you can use it, redistribute
+## it, and/or modify it under the terms of the GNU Lesser General
+## Public License as published by the Free Software Foundation; either
+## version 2.1 of the License, or (at your option) any later version.
+## The full text of the license can be found in the file LICENSE at
+## the top level of the deal.II distribution.
+##
+## ---------------------------------------------------------------------
+
+#
+# Try to find the Threading Building Blocks library
+#
+# This module exports
+#
+#   TBB_LIBRARIES
+#   TBB_INCLUDE_DIRS
+#   TBB_WITH_DEBUGLIB
+#
+
+MACRO(SET_IF_EMPTY _variable _value)
+  IF("${${_variable}}" STREQUAL "")
+    SET(${_variable} ${_value})
+  ENDIF()
+ENDMACRO()
+
+INCLUDE(FindPackageHandleStandardArgs)
+
+SET_IF_EMPTY(TBBROOT "$ENV{TBBROOT}")
+
+FIND_PATH(TBB_INCLUDE_DIR tbb/parallel_reduce.h
+  HINTS
+    ${TBBROOT}
+  PATH_SUFFIXES include include/tbb tbb
+  )
+
+FILE(GLOB _path ${TBBROOT}/build/*_release)
+
+FIND_LIBRARY(TBB_LIBRARY
+  NAMES tbb
+  HINTS
+    ${_path}
+    ${TBBROOT}
+  PATH_SUFFIXES lib${LIB_SUFFIX} lib64 lib
+  )
+
+FILE(GLOB _path ${TBBROOT}/build/*_debug)
+
+FIND_LIBRARY(TBB_DEBUG_LIBRARY
+  NAMES tbb_debug
+  HINTS
+    ${_path}
+    ${TBBROOT}
+  PATH_SUFFIXES lib${LIB_SUFFIX} lib64 lib
+  )
+
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(TBB DEFAULT_MSG
+  TBB_LIBRARY
+  TBB_INCLUDE_DIR
+  )
+
+MARK_AS_ADVANCED(
+  TBB_LIBRARY
+  TBB_DEBUG_LIBRARY
+  TBB_INCLUDE_DIR
+  )
+
+IF(TBB_FOUND)
+
+  IF(NOT TBB_DEBUG_LIBRARY MATCHES "-NOTFOUND")
+    SET(TBB_WITH_DEBUGLIB TRUE)
+    SET(TBB_LIBRARIES debug ${TBB_DEBUG_LIBRARY} optimized ${TBB_LIBRARY})
+  ELSE()
+    SET(TBB_LIBRARIES ${TBB_LIBRARY})
+  ENDIF()
+
+  SET(TBB_INCLUDE_DIRS ${TBB_INCLUDE_DIR})
+
+  MARK_AS_ADVANCED(TBBROOT)
+ELSE()
+  SET(TBBROOT "" CACHE PATH
+    "An optional hint to a TBB installation"
+    )
+ENDIF()
+
