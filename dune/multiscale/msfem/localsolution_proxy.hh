5a87e8679cf1ec9b66a4686fe029560cc1aae22c
Rene Milk <rene.milk@uni-muenster.de>
2014-02-04 15:21:15 +0100

    fixes local solution transfer

---
+++ dune/multiscale/msfem/localsolution_proxy.hh
@@ -0,0 +1,95 @@
+#ifndef DUNE_MULTISCALE_MSFEM_LOCALSOLUTION_PROXY_HH
+#define DUNE_MULTISCALE_MSFEM_LOCALSOLUTION_PROXY_HH
+
+//#include <dune/fem/function/common/dofblock.hh>
+#include <dune/fem/function/adaptivefunction.hh>
+
+namespace Dune {
+
+namespace Fem {
+template <class T, int c>
+class DofBlockProxy;
+template <class T>
+class Envelope ;
+}
+namespace Multiscale {
+namespace MsFEM {
+
+template <class T>
+class LocalsolutionProxy;
+
+//! Traits class for AdaptiveDiscreteFunction and AdaptiveLocalFunction
+template <class SearchType>
+struct LocalsolutionProxyTraits {
+  typedef MsFEMTraits::LocalGridDiscreteFunctionSpaceType DiscreteFunctionSpaceType;
+  typedef LocalsolutionProxy<SearchType> DiscreteFunctionType;
+  typedef MsFEMTraits::LocalGridDiscreteFunctionType ProxiedTraits;
+  typedef LocalsolutionProxyTraits<SearchType> Traits;
+  typedef typename ProxiedTraits::LocalFunctionStorageType LocalFunctionStorageType;
+  typedef typename ProxiedTraits :: LocalFunctionType LocalFunctionType;
+  typedef typename DiscreteFunctionSpaceType :: DomainType DomainType;
+  typedef typename DiscreteFunctionSpaceType :: RangeType RangeType;
+  typedef typename DiscreteFunctionSpaceType :: DomainFieldType DomainFieldType;
+  typedef typename DiscreteFunctionSpaceType :: RangeFieldType RangeFieldType;
+  typedef RangeFieldType DofType;
+  typedef typename DiscreteFunctionSpaceType :: JacobianRangeType JacobianRangeType;
+  typedef typename DiscreteFunctionSpaceType :: BlockMapperType BlockMapperType;
+  typedef typename DiscreteFunctionSpaceType :: GridType GridType;
+  typedef typename ProxiedTraits::MapperType MapperType;
+  typedef typename ProxiedTraits::DofStorageType  DofStorageType;
+  typedef typename ProxiedTraits::MutableDofStorageType MutableDofStorageType;
+  typedef typename DofStorageType :: DofIteratorType DofIteratorType;
+  typedef typename DofStorageType :: ConstDofIteratorType ConstDofIteratorType;
+  static const int blockSize = DiscreteFunctionSpaceType :: localBlockSize;
+  typedef typename ProxiedTraits::DofBlockType DofBlockType;
+  typedef typename ProxiedTraits::ConstDofBlockType ConstDofBlockType;
+  typedef typename ProxiedTraits::DofBlockPtrType DofBlockPtrType;
+  typedef typename ProxiedTraits::ConstDofBlockPtrType ConstDofBlockPtrType;
+}; // end struct LocalsolutionProxyTraits
+
+template <class SearchType>
+class LocalsolutionProxy
+    : public Dune::Fem::DiscreteFunctionInterface< LocalsolutionProxyTraits<SearchType> >
+{
+  typedef LocalsolutionProxyTraits<SearchType> TraitsType;
+  typedef Dune::Fem::DiscreteFunctionInterface<TraitsType> BaseType;
+  typedef CommonTraits::GridType::Traits::LeafIndexSet LeafIndexSetType;
+
+  typedef typename BaseType::LocalFunctionType LocalFunctionType;
+
+public:
+  typedef typename TraitsType::DofType DofType;
+  typedef std::map<typename LeafIndexSetType::IndexType, std::unique_ptr<DMM::MsFEMTraits::LocalGridDiscreteFunctionType>>
+    CorrectionsMapType;
+
+  LocalsolutionProxy(const CorrectionsMapType& corrections, const LeafIndexSetType& index_set,
+                     const MsFEMTraits::LocalGridDiscreteFunctionSpaceType& /*space_in*/, SearchType& search)
+    : corrections_(corrections)
+    , index_set_(index_set)
+    , search_(search)
+  {}
+
+  const LocalFunctionType localFunction ( const typename BaseType::EntityType &entity ) const {
+    const auto& coarse_cell = *search_.current_coarse_pointer();
+    auto it = corrections_.find(index_set_.index(coarse_cell));
+    if( it != corrections_.end())
+      return it->second->localFunction(entity);
+    assert(false);
+  }
+
+  virtual bool read_xdr ( const std :: string  ) {return false;}
+  virtual bool write_xdr ( const std :: string  ) const {return false;}
+  virtual bool read_ascii ( const std :: string  ) {return false;}
+  virtual bool write_ascii ( const std :: string  ) const {return false;}
+
+private:
+  const CorrectionsMapType& corrections_;
+  const LeafIndexSetType& index_set_;
+  SearchType& search_;
+};
+
+} // namespace MsFEM {
+} // namespace Multiscale {
+} // namespace Dune {
+
+#endif // DUNE_MULTISCALE_MSFEM_LOCALSOLUTION_PROXY_HH
--- dune/multiscale/msfem/msfem_solver.cc
+++ dune/multiscale/msfem/msfem_solver.cc
@@ -5,6 +5,9 @@
 #include <dune/common/timer.hh>
 #include <dune/multiscale/common/righthandside_assembler.hh>
 #include <dune/multiscale/msfem/coarse_scale_operator.hh>
+
+#include <dune/stuff/fem/functions/checks.hh>
+
 #include <dune/stuff/common/logging.hh>
 #include <dune/stuff/common/parameter/configcontainer.hh>
 #include <dune/stuff/common/profiler.hh>
@@ -15,42 +18,20 @@
 #include "dune/multiscale/common/dirichletconstraints.hh"
 #include "dune/multiscale/common/traits.hh"
 #include "dune/multiscale/msfem/msfem_traits.hh"
+#include <dune/multiscale/common/output_traits.hh>
 #include "msfem_solver.hh"
+#include "localsolution_proxy.hh"
 
 namespace Dune {
 namespace Multiscale {
 namespace MsFEM {
 
-template <class SearchType>
-class LocalsolutionProxy
-    : public MsFEMTraits::LocalGridDiscreteFunctionType
-{
-  typedef MsFEMTraits::LocalGridDiscreteFunctionType BaseType;
-  typedef CommonTraits::GridType::Traits::LeafIndexSet LeafIndexSetType;
-
-public:
-  typedef std::map<typename LeafIndexSetType::IndexType, std::unique_ptr<DMM::MsFEMTraits::LocalGridDiscreteFunctionType>>
-    CorrectionsMapType;
-
-  LocalsolutionProxy(const CorrectionsMapType& corrections, const LeafIndexSetType& index_set,
-                     const MsFEMTraits::LocalGridDiscreteFunctionSpaceType& space_in, SearchType& search)
-    : BaseType("corrections_proxy", space_in)
-    , corrections_(corrections)
-    , index_set_(index_set)
-    , search_(search)
-  {}
-
-  template <class FineEntity>
-  LocalFunctionType localFunction(const FineEntity& fine_entity) {
-    const auto& coarse_cell = *search_.current_coarse_pointer();
-    return corrections_[index_set_.index(coarse_cell)]->localFunction(fine_entity);
-  }
-
-private:
-  const CorrectionsMapType& corrections_;
-  const LeafIndexSetType& index_set_;
-  SearchType& search_;
+struct LocalOutputTraits {
+  //! --------- typedefs and classes for data output -----------------------------------------
+  typedef std::tuple<const MsFEMTraits::LocalGridDiscreteFunctionType*> IOTupleType;
+  typedef Dune::Fem::DataWriter<MsFEMTraits::LocalGridType, IOTupleType> DataOutputType;
 };
+
 
 void Elliptic_MsFEM_Solver::identify_fine_scale_part(LocalGridList& subgrid_list,
                                                      const DiscreteFunctionType& coarse_msfem_solution,
@@ -76,7 +57,7 @@ void Elliptic_MsFEM_Solver::identify_fine_scale_part(LocalGridList& subgrid_list
     auto& local_correction = *local_corrections[coarse_index];
     local_correction.clear();
     auto coarseSolutionLF = coarse_msfem_solution.localFunction(coarse_entity);
-    auto& tmp_local_storage = *localSolutions[0];
+//    auto& tmp_local_storage = *localSolutions[0];
 
     if (DSG::is_simplex_grid(coarse_space)) {
       BOOST_ASSERT_MSG(localSolutions.size() == Dune::GridSelector::dimgrid,
@@ -90,8 +71,9 @@ void Elliptic_MsFEM_Solver::identify_fine_scale_part(LocalGridList& subgrid_list
       for (int spaceDimension = 0; spaceDimension < Dune::GridSelector::dimgrid; ++spaceDimension) {
         *localSolutions[spaceDimension] *= grad_coarse_msfem_on_entity[0][spaceDimension];
         if (spaceDimension > 0)
-          tmp_local_storage += *localSolutions[spaceDimension];
+          local_correction += *localSolutions[spaceDimension];
       }
+      BOOST_ASSERT_MSG(false, "no adding of the boundary correctors??");
     } else {
       //! @warning At this point, we assume to have the same types of elements in the coarse and fine grid!
       BOOST_ASSERT_MSG(
@@ -101,47 +83,55 @@ void Elliptic_MsFEM_Solver::identify_fine_scale_part(LocalGridList& subgrid_list
       for (int dof = 0; dof < coarseSolutionLF.numDofs(); ++dof) {
         *localSolutions[dof] *= coarseSolutionLF[dof];
         if (dof > 0)
-          tmp_local_storage += *localSolutions[dof];
+          local_correction += *localSolutions[dof];
+      }
+
+      // oversampling : restrict the local correctors to the element T
+      // ie set all dofs not "covered" by the coarse cell to 0
+      if(DSC_CONFIG_GET("msfem.oversampling_layers", 0)) {
+        for (auto& local_entity : localSolManager.space()) {
+          const auto& lg_points = localSolManager.space().lagrangePointSet(local_entity);
+          const auto& reference_element = DSG::reference_element(coarse_entity);
+          const auto& coarse_geometry = coarse_entity.geometry();
+          auto entity_local_correction = local_correction.localFunction(local_entity);
+
+          for (const auto lg_i : DSC::valueRange(int(lg_points.size()))) {
+            const auto global_lg_point = local_entity.geometry().global(lg_points.point(lg_i));
+            const bool covered = reference_element.checkInside(coarse_geometry.local(global_lg_point));
+            entity_local_correction[lg_i] = covered ? entity_local_correction[lg_i] : RangeType::value_type(0);
+          }
+        }
       }
 
       // add dirichlet corrector
       local_correction += *localSolutions[coarseSolutionLF.numDofs() + 1];
       // substract neumann corrector
       local_correction -= *localSolutions[coarseSolutionLF.numDofs()];
-    }
-
-    // oversampling strategy 1 or 2: restrict the local correctors to the element T
-    // ie set all dofs not "covered" by the coarse cell to 0
-    if(DSC_CONFIG_GET("msfem.oversampling_layers", 0)) {
-      for (auto& local_entity : localSolManager.space()) {
-        const auto& lg_points = localSolManager.space().lagrangePointSet(local_entity);
-        const auto& reference_element = DSG::reference_element(coarse_entity);
-        const auto& coarse_geometry = coarse_entity.geometry();
-        auto entity_local_correction = local_correction.localFunction(local_entity);
-
-        for (const auto lg_i : DSC::valueRange(int(lg_points.size()))) {
-          const auto global_lg_point = local_entity.geometry().global(lg_points.point(lg_i));
-          const bool covered = reference_element.checkInside(coarse_geometry.local(global_lg_point));
-          entity_local_correction[lg_i] = covered ? entity_local_correction[lg_i] : RangeType::value_type(0);
-        }
+      if (DSC_CONFIG_GET("msfem.local_corrections_vtk_output", false)) {
+        LocalOutputTraits::IOTupleType coarse_grid_series(&local_correction);
+        const std::string name = (boost::format("local_correction_%d_") % coarse_index).str();
+        Dune::Multiscale::OutputParameters outputparam;
+        outputparam.set_prefix(name);
+        auto& grid = localSolManager.space().gridPart().grid();
+        LocalOutputTraits::DataOutputType(grid, coarse_grid_series, outputparam).writeData(1.0 /*dummy*/, name);
       }
     }
   }
 
-  DSC_LOG_INFO << "Identifying fine scale part of the MsFEM solution... ";
   SearchType search(coarse_space, subgrid_list);
   auto proxybase_gridpart = subgrid_list.gridPart(0);
   MsFEMTraits::LocalGridDiscreteFunctionSpaceType proxybase_space(proxybase_gridpart);
   ProxyType proxy(local_corrections, coarse_indexset, proxybase_space, search);
   ProjectionType::project(proxy, fine_scale_part, search);
-  DSC_LOG_INFO << " done." << std::endl;
   BOOST_ASSERT_MSG(fine_scale_part.dofsValid(), "Fine scale part DOFs need to be valid!");
 }
 
-void Elliptic_MsFEM_Solver::apply(const CommonTraits::DiscreteFunctionSpaceType &coarse_space, const CommonTraits::DiffusionType& diffusion_op,
+void Elliptic_MsFEM_Solver::apply(const CommonTraits::DiscreteFunctionSpaceType &coarse_space,
+                                  const CommonTraits::DiffusionType& diffusion_op,
                                   const CommonTraits::FirstSourceType& f,
-    DiscreteFunctionType& coarse_scale_part, DiscreteFunctionType& fine_scale_part,
-    DiscreteFunctionType& solution) const {
+                                  DiscreteFunctionType& coarse_scale_part,
+                                  DiscreteFunctionType& fine_scale_part,
+                                  DiscreteFunctionType& solution) const {
   if (DSC_CONFIG_GET("msfem.petrov_galerkin", 1))
     DSC_LOG_ERROR << "MsFEM does not work with Petrov-Galerkin at the moment!\n";
 
