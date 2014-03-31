72a43668c320a9797f6e11d80c499b0678164ad0
Sven Kaulmann <sven.kaulmann@mathematik.uni-stuttgart.de>
2014-03-31 16:20:02 +0200

    Hardcoded fine mesh corners to coarse mesh corners
    Dunno why coarse mesh corners are hardcoded but
    the current code made the fine mesh not cover
    the whole domain as on each process, a fine mesh
    was created that used the corners of the load-
    balanced coarse mesh which are not the same as
    those of the non-load-balanced.

--- dune/multiscale/common/grid_creation.cc
+++ dune/multiscale/common/grid_creation.cc
@@ -10,20 +10,22 @@ std::pair<std::shared_ptr<Dune::Multiscale::CommonTraits::GridType>,
 Dune::Multiscale::make_grids() {
   const int dim_world = CommonTraits::GridType::dimensionworld;
   typedef FieldVector<typename CommonTraits::GridType::ctype, dim_world> CoordType;
+  CoordType lowerLeft(0.0);
+  CoordType upperRight(1.0);
+
   const auto coarse_cells = DSC_CONFIG_GET("global.macro_cells_per_dim", 8);
   array<unsigned int, dim_world> elements;
   for (const auto i : DSC::valueRange(dim_world))
     elements[i] = coarse_cells;
   auto coarse_gridptr =
-      StructuredGridFactory<CommonTraits::GridType>::createCubeGrid(CoordType(0.0), CoordType(1.0), elements);
+      StructuredGridFactory<CommonTraits::GridType>::createCubeGrid(lowerLeft, upperRight, elements);
 
-  const auto coarse_dimensions = DSG::dimensions(*coarse_gridptr);
-  CoordType lowerLeft(0);
-  CoordType upperRight(0);
+//  const auto coarse_dimensions = DSG::dimensions(*coarse_gridptr);
+
   for (const auto i : DSC::valueRange(dim_world)) {
     elements[i] = coarse_cells * DSC_CONFIG_GET("global.micro_cells_per_macrocell_dim", 8);
-    lowerLeft[i] = coarse_dimensions.coord_limits[i].min();
-    upperRight[i] = coarse_dimensions.coord_limits[i].max();
+//    lowerLeft[i] = coarse_dimensions.coord_limits[i].min();
+//    upperRight[i] = coarse_dimensions.coord_limits[i].max();
   }
   auto fine_gridptr = StructuredGridFactory<CommonTraits::GridType>::createCubeGrid(lowerLeft, upperRight, elements);
   return {coarse_gridptr, fine_gridptr};
