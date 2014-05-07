f16f75b22bafac230d73dd9e74043965ae0cfd33
Sven Kaulmann <sven.kaulmann@mathematik.uni-stuttgart.de>
2014-05-07 17:32:43 +0200

    make assemble non-static and rename

--- dune/multiscale/common/righthandside_assembler.hh
+++ dune/multiscale/common/righthandside_assembler.hh
@@ -10,10 +10,8 @@
 
 namespace Dune {
 namespace Multiscale {
-
 namespace MsFEM {
 class LocalGridList;
-} // namespace MsFEM
 
 /** Assembler for right rand side
  * We assemble the right hand side in a LSE, i.e. f \cdot \Phi_H + G \cdot \nabala \Phi_H
@@ -30,15 +28,16 @@ public:
    *  assemble-method for MsFEM in symmetric (non-Petrov-Galerkin) formulation
    *  rhsVector is the output parameter (kind of return value)
    **/
-  static void assemble_msfem(const CommonTraits::DiscreteFunctionSpaceType& coarse_space,
-                             const CommonTraits::SourceType& f, MsFEM::LocalGridList& subgrid_list,
-                             CommonTraits::DiscreteFunctionType& rhsVector);
+  void assemble(const CommonTraits::DiscreteFunctionSpaceType& coarse_space,
+                      const CommonTraits::SourceType& f, MsFEM::LocalGridList& subgrid_list,
+                      CommonTraits::DiscreteFunctionType& rhsVector);
 private:
   static std::vector<std::vector<RangeType>> coarseBaseEvals_;
   static std::vector<std::vector<JacobianRangeType>> coarseBaseJacs_;
   static bool cached_;
 
 }; // end class
+} // namespace MsFEM
 } // end namespace Multiscale
 } // end namespace Dune
 
