2b7c69742be123d7ad63a9348b26b88f4604e946
Sven Kaulmann <sven.kaulmann@mathematik.uni-stuttgart.de>
2014-05-27 11:32:52 +0200

    Added warning and nice level to submitrange

--- submitrange.sh
+++ submitrange.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+echo "Logging-level richtig eingestellt?"
 
 for (( c=$1; c<=$2; c*=2 ))
 do
     numnodes=$((($c+12-1)/12))
-    qsub -l walltime=00:00:30,nodes=$numnodes:westmere:ppn=12 -t $c ../test_scaleup.cmd
+    qsub -l nice=-18,walltime=01:00:00,nodes=$numnodes:westmere:ppn=12 -t $c ../test_scaleup.cmd
 
 done
