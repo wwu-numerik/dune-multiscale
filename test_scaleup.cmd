9b778c12d4c6b452aa51da20a6fec0f955c295c9
Sven Kaulmann <sven.kaulmann@mathematik.uni-stuttgart.de>
2014-06-17 16:28:18 +0200

    Updated test_scaleup command script

--- test_scaleup.cmd
+++ test_scaleup.cmd
@@ -1,20 +1,18 @@
-#PBS -l walltime=00:00:20,nodes=2:westmere:ppn=12
+#PBS -l nice=-18,walltime=03:00:00,nodes=11:ppn=12
 #PBS -A o0num
-#PBS -M s_kaul01@uni-muenster.de
 #PBS -m ae
+#PBS -M s_kaul01@uni-muenster.de
 #PBS -q math
-#PBS -N test_scaleup_128_466
+#PBS -N week_21_tests_S
 #PBS -j oe
-#PBS -t 16
+# #PBS -t 32
 #PBS -o data/${PBS_JOBNAME%-*}/$PBS_JOBID/logdata/runlog.log
 cd $PBS_O_WORKDIR
 mkdir -p data/${PBS_JOBNAME%-*}/$PBS_JOBID/logdata/
-# export LD_LIBRARY_PATH=/Applic.PALMA/compiler/gcc/4.7.2/lib64:$LD_LIBRARY_PATH
-### ${PBS_JOBNAME%-*} deletes "-" and the array-id from the jobname
 
-ulimit -q 13107200
-time mpiexec -np $PBS_ARRAYID ./elliptic_msfem ../build/msfem.param -global.datadir data/${PBS_JOBNAME%-*}/$PBS_JOBID
+MPIHOME=/Applic.PALMA/mpi/openmpi/gcc4.7.2/1.6.3 likwid-mpirun  -d -mpi=openmpi -np $PBS_ARRAYID  -nperdomain N:12 -- "./elliptic_msfem ../build/msfem.param -global.datadir data/${PBS_JOBNAME%-*}/$PBS_JOBID"
+
+# mpiexec -np $PBS_ARRAYID ./elliptic_msfem ../build/msfem.param -global.datadir data/${PBS_JOBNAME%-*}/$PBS_JOBID
+# mpirun -np $PBS_ARRAYID amplxe-cl -result-dir data/$PBS_JOBID/ampl-xe -collect hotspots -- ./elliptic_msfem ../build/msfem.param
 
 
-# mpiexec -np $PBS_ARRAYID /scratch/tmp/s_kaul01/external/bin/hpcrun -o data/${PBS_JOBNAME%-*}/$PBS_JOBID/hpctool_measurements ./elliptic_msfem ../build/msfem.param -global.datadir data/${PBS_JOBNAME%-*}/$PBS_JOBID 
-# /scratch/tmp/s_kaul01/external/bin/hpcprof -M stats -S elliptic_msfem.hpcstruct -I ../dune/+ -I ../src/+ -I ../../dune-fem/dune/+ -I ../../dune-spgrid/dune/+ -I ../../dune-common/dune/+ -I ../../dune-grid/dune/+ -I ../../dune-localfunctions/dune/+ -I ../../dune-stuff/dune/+ -I ../../dune-geometry/dune/+ -I ../../dune-istl/dune/+ -o data/${PBS_JOBNAME%-*}/$PBS_JOBID/hpctool_database data/${PBS_JOBNAME%-*}/$PBS_JOBID/hpctool_measurements/
