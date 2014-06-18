d2a9de0dca8b90c4b0365d975a148edfa04b62cc
Rene Milk <rene.milk@uni-muenster.de>
2014-06-18 23:45:55 +0200

    fixes a couple of mem scoping issues

--- dune/multiscale/tools/discretefunctionwriter.hh
+++ dune/multiscale/tools/discretefunctionwriter.hh
@@ -101,8 +101,8 @@ class DiscreteFunctionIO : public boost::noncopyable {
      */
     MemoryBackend(const GridViewPtrType& grid_view, const std::string filename = "nonsense_default_for_map")
       : dir_(boost::filesystem::path(DSC_CONFIG_GET("global.datadir", "data")) / filename)
-      , space_(DMM::MsFEMTraits::SpaceProviderType::create(DMM::MsFEMTraits::LocalGridProviderType(&grid_view->grid()),
-                                                           CommonTraits::st_gdt_grid_level))
+      , local_grid_provider_(&grid_view->grid())
+      , space_(DMM::MsFEMTraits::SpaceProviderType::create(local_grid_provider_, CommonTraits::st_gdt_grid_level))
     {}
 
     void append(const DiscreteFunction_ptr& df) { functions_.push_back(df); }
@@ -119,6 +119,7 @@ class DiscreteFunctionIO : public boost::noncopyable {
 
   private:
     const boost::filesystem::path dir_;
+    const DMM::MsFEMTraits::LocalGridProviderType local_grid_provider_;
     DiscreteFunctionSpaceType space_;
     Vector functions_;
   };
