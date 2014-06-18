04fff9b22eab5407bf9c19eeb1eda0154ed88e6e
Rene Milk <rene.milk@uni-muenster.de>
2014-06-18 13:30:17 +0200

    prelim. local operator -> gdt

--- dune/multiscale/msfem/localproblems/localoperator.cc
+++ dune/multiscale/msfem/localproblems/localoperator.cc
@@ -24,11 +24,122 @@
 #include <dune/gdt/operators/prolongations.hh>
 #include <dune/gdt/spaces/constraints.hh>
 #include <dune/gdt/functionals/l2.hh>
+#include <dune/stuff/common/exceptions.hh>
 
 namespace Dune {
+
+// forward, to be used in the traits
+template< class LocalizableFunctionImp >
+class CoarseBasisProduct;
+
+
+/**
+ *  \brief Traits for the Product evaluation.
+ */
+template< class LocalizableFunctionImp >
+class CoarseBasisProductTraits
+{
+public:
+  typedef CoarseBasisProduct< LocalizableFunctionImp > derived_type;
+  typedef LocalizableFunctionImp            LocalizableFunctionType;
+  static_assert(std::is_base_of< Dune::Stuff::IsLocalizableFunction, LocalizableFunctionImp >::value,
+                "LocalizableFunctionImp has to be derived from Stuff::IsLocalizableFunction.");
+};
+
+
+template< class LocalizableFunctionImp >
+class CoarseBasisProduct
+  : public GDT::LocalEvaluation::Codim0Interface< CoarseBasisProductTraits< LocalizableFunctionImp >, 1 >
+{
+public:
+  typedef CoarseBasisProductTraits< LocalizableFunctionImp >   Traits;
+  typedef typename Traits::LocalizableFunctionType  LocalizableFunctionType;
+
+  CoarseBasisProduct(const LocalizableFunctionType& inducingFunction)
+    : inducingFunction_(inducingFunction)
+  {}
+
+  template< class EntityType >
+  class LocalfunctionTuple
+  {
+    typedef typename LocalizableFunctionType::LocalfunctionType LocalfunctionType;
+  public:
+    typedef std::tuple< std::shared_ptr< LocalfunctionType > > Type;
+  };
+
+  template< class EntityType >
+  typename LocalfunctionTuple< EntityType >::Type localFunctions(const EntityType& entity) const
+  {
+    return std::make_tuple(inducingFunction_.local_function(entity));
+  }
+
+  /**
+   * \brief extracts the local functions and calls the correct order() method
+   */
+  template< class E, class D, int d, class R, int rT, int rCT >
+  size_t order(const typename LocalfunctionTuple< E >::Type& localFuncs,
+               const Stuff::LocalfunctionSetInterface< E, D, d, R, rT, rCT >& testBase) const
+  {
+    const auto localFunction = std::get< 0 >(localFuncs);
+    return order(*localFunction, testBase);
+  }
+
+  /**
+   *  \todo add copydoc
+   *  \return localFunction.order() + testBase.order()
+   */
+  template< class E, class D, int d, class R, int rL, int rCL, int rT, int rCT >
+  size_t order(const Stuff::LocalfunctionInterface< E, D, d, R, rL, rCL >& localFunction,
+               const Stuff::LocalfunctionSetInterface< E, D, d, R, rT, rCT >& testBase) const
+  {
+    return localFunction.order() + testBase.order();
+  } // int order(...)
+
+  /**
+   * \brief extracts the local functions and calls the correct evaluate() method
+   */
+  template< class E, class D, int d, class R, int rT, int rCT >
+   void evaluate(const typename LocalfunctionTuple< E >::Type& localFuncs,
+                 const Stuff::LocalfunctionSetInterface< E, D, d, R, rT, rCT >& testBase,
+                 const Dune::FieldVector< D, d >& localPoint,
+                 Dune::DynamicVector< R >& ret) const
+  {
+    const auto localFunction = std::get< 0 >(localFuncs);
+    evaluate(*localFunction, testBase, localPoint, ret);
+  }
+
+  template< class E, class D, int d, class R, int rL, int rCL, int rT, int rCT >
+  void evaluate(const Stuff::LocalfunctionInterface< E, D, d, R, rL, rCL >& localFunction,
+                const Stuff::LocalfunctionSetInterface< E, D, d, R, rT, rCT >& testBase,
+                const Dune::FieldVector< D, d >& localPoint,
+                Dune::DynamicVector< R >& ret) const
+  {
+    typedef Dune::FieldVector< R, 1 > RangeType;
+    // evaluate local function
+    DUNE_THROW(NotImplemented, "wrong eval still");
+    const auto functionValue = localFunction.evaluate(localPoint);
+    // evaluate test base
+    const size_t size = testBase.size();
+    std::vector< RangeType > testValues(size, RangeType(0));
+    testBase.evaluate(localPoint, testValues);
+    // compute product
+    assert(ret.size() >= size);
+    for (size_t ii = 0; ii < size; ++ii) {
+      ret[ii] = functionValue[0] * testValues[ii];
+    }
+  }
+
+
+private:
+  const LocalizableFunctionType& inducingFunction_;
+}; // class Product
+
+
+
 namespace Multiscale {
 namespace MsFEM {
-  
+
+
 LocalProblemOperator::LocalProblemOperator(const CoarseSpaceType& coarse_space,
                                            const LocalGridDiscreteFunctionSpaceType& space,
                                            const DiffusionOperatorType& diffusion_op)
@@ -102,20 +213,19 @@ void LocalProblemOperator::assemble_all_local_rhs(const CoarseEntityType& coarse
   const auto numInnerCorrectors = allLocalRHS.size() - numBoundaryCorrectors;
   //!*********** anfang neu gdt
 
+  if (is_simplex_grid)
+      DUNE_THROW(NotImplemented, "special treatment for simplicial grids missing");
+
   std::size_t coarseBaseFunc = 0;
   for (; coarseBaseFunc < numInnerCorrectors; ++coarseBaseFunc)
   {
-    if (is_simplex_grid) {
-//      diffusionsauswertung in unitVectors[coarseBaseFunc]
-    } else {
-//      diffusionsauswertung in
-//      const DomainType quadInCoarseLocal = coarseEntity.geometry().local(QuadraturPunkt);
-//      coarseBaseSet.jacobianAll(quadInCoarseLocal, coarseBaseFuncJacs);
-    }
-//    baseSet.jacobianAll(quadrature[quadraturePoint], gradient_phi);
-//    for (unsigned int i = 0; i < numBaseFunctions; ++i) {
-//      rhsLocalFunction[i] -= weight * (diffusions_auswertung * gradient_phi[i][0]);
-//    }
+    typedef CoarseBasisProduct<LocalDiffusionType> EvaluationType;
+    auto& rhs_vector = allLocalRHS[coarseBaseFunc]->vector();
+    GDT::Functionals::L2Volume< LocalDiffusionType, CommonTraits::GdtVectorType,
+        MsFEMTraits::LocalSpaceType, MsFEMTraits::LocalGridViewType,
+        EvaluationType > rhs_functional(local_diffusion_operator_, rhs_vector, localSpace_);
+    system_assembler_.add(rhs_functional);
+
   }
 
   coarseBaseFunc++; // coarseBaseFunc == numInnerCorrectors
