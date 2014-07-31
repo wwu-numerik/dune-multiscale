be29d83fce7ae8d1b753b57d8c0fe2b471205a9e
Rene Milk <rene.milk@uni-muenster.de>
2014-07-31 11:08:41 +0200

    [tests] adds new one for grid matching

--- dune/multiscale/test/compare_msfem.cc
+++ dune/multiscale/test/compare_msfem.cc
@@ -1,13 +1,5 @@
-#include <config.h>
-#include <config.h>
+#include <dune/multiscale/test/test_common.hh>
 
-#include <dune/stuff/test/test_common.hh>
-#include <gtest.h>
-
-#include <string>
-#include <array>
-#include <initializer_list>
-#include <vector>
 #include <dune/stuff/common/fixed_map.hh>
 #include <dune/stuff/common/string.hh>
 #include <dune/stuff/common/ranges.hh>
@@ -20,22 +12,17 @@
 
 #include <boost/filesystem.hpp>
 
-using namespace Dune::Stuff::Common;
-
-void set_param();
-
-std::string prepend_test_dir(std::string fn)
-{
-  boost::filesystem::path path(st_testdata_directory);
-  path /= fn;
-  return path.string();
-}
 
 TEST(MSFEM, All) {
   using namespace Dune::Multiscale;
   using namespace Dune::Multiscale::MsFEM;
 
-  set_param();
+  set_param({{"grids.macro_cells_per_dim", "[4;4;4]"},
+      {"micro_cells_per_macrocell_dim", "[8;8;8]"},
+      {"msfem.oversampling_layers", "0"},
+      {"msfem.fem_comparison", "0"},
+      {"global.vtk_output", "0"},
+      {"problem.name", "Nine"},});
 
   const auto& errorsMap = algorithm();
   
@@ -53,11 +40,3 @@ int main(int argc, char** argv) {
   return RUN_ALL_TESTS();
 }
 
-void set_param() {
-  DSC_CONFIG.set("grids.macro_cells_per_dim", "[4;4;4]");
-  DSC_CONFIG.set("micro_cells_per_macrocell_dim", "[8;8;8]");
-  DSC_CONFIG.set("msfem.oversampling_layers", 0);
-  DSC_CONFIG.set("msfem.fem_comparison", 0);
-  DSC_CONFIG.set("global.vtk_output", 0);
-  DSC_CONFIG.set("problem.name", "Nine");
-}
---
+++ dune/multiscale/test/test_common.hh
@@ -0,0 +1,78 @@
+#ifndef DUNE_MULTISCALE_TEST_COMMON_HH
+#define DUNE_MULTISCALE_TEST_COMMON_HH
+
+#include <config.h>
+
+#include <dune/stuff/test/test_common.hh>
+#include <gtest.h>
+
+#include <string>
+#include <array>
+#include <initializer_list>
+#include <vector>
+
+#include <dune/stuff/common/ranges.hh>
+#include <dune/stuff/grid/information.hh>
+#include <dune/stuff/common/parameter/configcontainer.hh>
+
+#include <dune/multiscale/common/grid_creation.hh>
+
+#include <boost/filesystem.hpp>
+
+
+using namespace Dune::Stuff::Common;
+using namespace Dune::Multiscale;
+using namespace std;
+
+
+string prepend_test_dir(string fn)
+{
+  boost::filesystem::path path(st_testdata_directory);
+  path /= fn;
+  return path.string();
+}
+
+void set_param(map<string, string> params) {
+  for( auto vp : params) {
+    DSC_CONFIG.set(vp.first, vp.second);
+  }
+}
+
+class GridTestBase : public ::testing::TestWithParam<map<string,string>> {
+
+public:
+
+ virtual void SetUp() {
+    set_param(GetParam());
+    grids_ = make_grids();
+  }
+ virtual void TearDown() {
+   grids_ = decltype(grids_)();
+ }
+
+protected:
+ decltype(make_grids()) grids_;
+};
+
+static const map<string, string> p_small = {{"grids.macro_cells_per_dim", "[4;4;4]"}
+                                           ,{"micro_cells_per_macrocell_dim", "[8;8;8]"}
+                                           ,{"msfem.oversampling_layers", "0"}
+                                           };
+static const map<string, string> p_large = {{"grids.macro_cells_per_dim", "[20;20;20]"}
+                                           ,{"micro_cells_per_macrocell_dim", "[40;40;40]"}
+                                           ,{"msfem.oversampling_layers", "0"}
+                                           };
+static const map<string, string> p_aniso = {{"grids.macro_cells_per_dim", "[14;4;6]"}
+                                           ,{"micro_cells_per_macrocell_dim", "[3;32;8]"}
+                                           ,{"msfem.oversampling_layers", "0"}
+                                           };
+static const map<string, string> p_wover = {{"grids.macro_cells_per_dim", "[14;14;14]"}
+                                           ,{"micro_cells_per_macrocell_dim", "[18;18;18]"}
+                                           ,{"msfem.oversampling_layers", "6"}
+                                           };
+static const map<string, string> p_huge  = {{"grids.macro_cells_per_dim", "[40;40;40]"}
+                                           ,{"micro_cells_per_macrocell_dim", "[120;120;120]"}
+                                           ,{"msfem.oversampling_layers", "0"}
+                                           };
+
+#endif // DUNE_MULTISCALE_TEST_COMMON_HH
