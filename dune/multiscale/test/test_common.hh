bbb09dcf976b83931ad67bf44792dcec4183e977
Rene Milk <rene.milk@uni-muenster.de>
2014-08-05 18:20:43 +0200

    [tests] lots more unittest

--- dune/multiscale/test/test_common.hh
+++ dune/multiscale/test/test_common.hh
@@ -42,11 +42,11 @@ class GridTestBase : public ::testing::TestWithParam<map<string,string>> {
 
 public:
 
- virtual void SetUp() {
+ GridTestBase() {
     set_param(GetParam());
     grids_ = make_grids();
   }
- virtual void TearDown() {
+ virtual ~GridTestBase() {
    grids_ = decltype(grids_)();
  }
 
@@ -55,27 +55,27 @@ protected:
 };
 
 static const map<string, string> p_small = {{"grids.macro_cells_per_dim", "[4;4;4]"}
-                                           ,{"micro_cells_per_macrocell_dim", "[8;8;8]"}
+                                           ,{"grids.micro_cells_per_macrocell_dim", "[8;8;8]"}
                                            ,{"msfem.oversampling_layers", "0"}
                                            };
 static const map<string, string> p_large = {{"grids.macro_cells_per_dim", "[20;20;20]"}
-                                           ,{"micro_cells_per_macrocell_dim", "[40;40;40]"}
+                                           ,{"grids.micro_cells_per_macrocell_dim", "[40;40;40]"}
                                            ,{"msfem.oversampling_layers", "0"}
                                            };
 static const map<string, string> p_aniso = {{"grids.macro_cells_per_dim", "[14;4;6]"}
-                                           ,{"micro_cells_per_macrocell_dim", "[3;32;8]"}
+                                           ,{"grids.micro_cells_per_macrocell_dim", "[3;32;8]"}
                                            ,{"msfem.oversampling_layers", "0"}
                                            };
 static const map<string, string> p_wover = {{"grids.macro_cells_per_dim", "[14;14;14]"}
-                                           ,{"micro_cells_per_macrocell_dim", "[18;18;18]"}
+                                           ,{"grids.micro_cells_per_macrocell_dim", "[18;18;18]"}
                                            ,{"msfem.oversampling_layers", "6"}
                                            };
 static const map<string, string> p_huge  = {{"grids.macro_cells_per_dim", "[40;40;40]"}
-                                           ,{"micro_cells_per_macrocell_dim", "[120;120;120]"}
+                                           ,{"grids.micro_cells_per_macrocell_dim", "[120;120;120]"}
                                            ,{"msfem.oversampling_layers", "0"}
                                            };
 static const map<string, string> p_fail  = {{"grids.macro_cells_per_dim", "[12;15;10]"}
-                                           ,{"micro_cells_per_macrocell_dim", "[6;7;10]"}
+                                           ,{"grids.micro_cells_per_macrocell_dim", "[6;7;10]"}
                                            ,{"msfem.oversampling_layers", "0"}
                                            };
 #endif // DUNE_MULTISCALE_TEST_COMMON_HH
