d77ba8ed19da13e0e66dfdf2528f82d988913cbb
Rene Milk <rene.milk@uni-muenster.de>
2014-08-07 12:09:22 +0200

    [tests] parameterize msfem comparison

--- dune/multiscale/test/compare_msfem.cc
+++ dune/multiscale/test/compare_msfem.cc
@@ -9,31 +9,45 @@
 #include <dune/multiscale/common/error_container.hh>
 #include <dune/multiscale/msfem/algorithm.hh>
 #include <dune/multiscale/problems/selector.hh>
+#include <dune/stuff/common/fixed_map.hh>
 
 #include <boost/filesystem.hpp>
 
+//          (Config key -> value, error name -> max value)
+typedef pair< map<string,string>, FixedMap<string,double,2>> TestArgs;
 
-TEST(MSFEM, All) {
+
+const TestArgs m_small{p_small, {{"msfem_exact_L2", 0.14}, {"msfem_exact_H1", 2.3}}};
+const TestArgs m_large{p_large, {{"msfem_exact_L2", 0.07}, {"msfem_exact_H1", 1.15}}};
+
+struct MsFemCompare : public ::testing::TestWithParam<TestArgs> {
+
+  MsFemCompare()
+  {
+    set_param(this->GetParam().first);
+    set_param({{"msfem.fem_comparison", "0"},
+        {"global.vtk_output", "0"},
+        {"problem.name", "Nine"}});
+  }
+};
+
+TEST_P(MsFemCompare, All) {
   using namespace Dune::Multiscale;
   using namespace Dune::Multiscale::MsFEM;
 
-  set_param({{"grids.macro_cells_per_dim", "[4;4;4]"},
-      {"micro_cells_per_macrocell_dim", "[8;8;8]"},
-      {"msfem.oversampling_layers", "0"},
-      {"msfem.fem_comparison", "0"},
-      {"global.vtk_output", "0"},
-      {"problem.name", "Nine"},});
-
-  const auto& errorsMap = algorithm();
+  const auto errorsMap = algorithm();
   
+  const auto& expected_errors = this->GetParam().second;
   auto found = errorsMap.find("msfem_exact_L2"); 
-  ASSERT_TRUE(found!=errorsMap.end());
-  EXPECT_GT(double(0.14), found->second);
+  EXPECT_NE(found,errorsMap.end());
+  EXPECT_GT(expected_errors["msfem_exact_L2"], found->second);
   found = errorsMap.find("msfem_exact_H1");
-  ASSERT_TRUE(found!=errorsMap.end());
-  EXPECT_GT(double(2.3), found->second);
+  EXPECT_NE(found, errorsMap.end());
+  EXPECT_GT(expected_errors["msfem_exact_H1"], found->second);
 
 }
+
+INSTANTIATE_TEST_CASE_P( MsFemComparisons, MsFemCompare, testing::Values(m_small, m_large));
 
 int main(int argc, char** argv) {
   test_init(argc, argv);
