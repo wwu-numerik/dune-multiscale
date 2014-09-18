2e659a051522df9aa19106c0fb584f96f664de26
Rene Milk <rene.milk@uni-muenster.de>
2014-09-18 13:50:29 +0200

    [cmake] find tbb gt moved to dune-stuff

--- cmake/modules/DuneMultiscaleMacros.cmake
+++ cmake/modules/DuneMultiscaleMacros.cmake
@@ -3,9 +3,10 @@ message(AUTHOR_WARNING "TODO: Implement module test.")
 find_package(PETSc COMPONENTS CXX )
 
 find_package(METIS)
-find_package(ParMETIS)
+find_package(ParMETIS REQUIRED)
 include(cmake/modules/AddMETISFlags.cmake)
 include(AddParMETISFlags)
 include(CheckEmplace)
 
-find_package(Eigen3 REQUIRED)
+find_package(Eigen3 3.2.0 REQUIRED)
+find_package(TBB REQUIRED)
