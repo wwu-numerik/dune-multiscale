cb44f53286004d5e59606a699448fab9dd62601b
Rene Milk <rene.milk@uni-muenster.de>
2014-09-19 15:02:42 +0200

    cleanup

--- dune/multiscale/msfem/proxygridview.cc
+++ dune/multiscale/msfem/proxygridview.cc
@@ -8,7 +8,9 @@ namespace Multiscale {
 
 ProxyGridview::ProxyGridview(const LocalGridList& localGrids)
   : BaseType(localGrids.getSubGrid(0))
-  , localGrids_(localGrids) {}
+  , localGrids_(localGrids) {
+  localGrids_.size();
+}
 
 } // namespace Dune
 } // namespace Multiscale
