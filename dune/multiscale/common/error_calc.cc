73e952e6594084f8fc18f030e494fc10a6cc5ae4
Rene Milk <rene.milk@uni-muenster.de>
2014-09-29 16:30:47 +0200

    only use one grid traversal to calculate all errors

--- dune/multiscale/common/error_calc.cc
+++ dune/multiscale/common/error_calc.cc
@@ -6,6 +6,7 @@
 #include <boost/filesystem/fstream.hpp>
 #include <iostream>
 #include <map>
+#include <unordered_map>
 #include <memory>
 #include <string>
 #include <utility>
@@ -29,6 +30,15 @@
 #include <dune/stuff/discretefunction/projection/heterogenous.hh>
 
 using namespace Dune::Multiscale;
+namespace DGP = Dune::GDT::Products;
+typedef DSFu::Difference<Problem::ExactSolutionType, CommonTraits::ConstDiscreteFunctionType> DifferenceType;
+typedef DSFu::Difference<CommonTraits::ConstDiscreteFunctionType,
+                                     CommonTraits::ConstDiscreteFunctionType> DiscreteDifferenceType;
+typedef DGP::L2Localizable<CommonTraits::GridViewType, DifferenceType> L2ErrorAnalytical;
+typedef DGP::L2Localizable<CommonTraits::GridViewType, DiscreteDifferenceType> L2ErrorDiscrete;
+typedef DGP::H1SemiLocalizable<CommonTraits::GridViewType, DifferenceType> H1sErrorAnalytical;
+typedef DGP::H1SemiLocalizable<CommonTraits::GridViewType, DiscreteDifferenceType> H1sErrorDiscrete;
+typedef DGP::L2Localizable<CommonTraits::GridViewType, CommonTraits::ConstDiscreteFunctionType> DiscreteL2;
 
 void solution_output(const CommonTraits::DiscreteFunctionType& msfem_solution) {
   using namespace Dune;
@@ -69,13 +79,11 @@ ErrorCalculator::ErrorCalculator(const std::unique_ptr<LocalsolutionProxy> &msfe
 }
 
 std::map<std::string, double> Dune::Multiscale::ErrorCalculator::print(std::ostream& out) {
-  using namespace Dune::GDT::Products;
+  using namespace std;
   out << std::endl << "The L2 errors:" << std::endl << std::endl;
 
   const size_t over_integrate = 0; // <- would let the product use a higher quadrature order than needed
 
-  typedef Stuff::Functions::Difference<Problem::ExactSolutionType, CommonTraits::ConstDiscreteFunctionType>
-  DifferenceType;
   /// TODO only call assemble once
   const auto fine_grid = make_grids().second;
   CommonTraits::GridProviderType fine_grid_provider(*fine_grid);
@@ -97,78 +105,84 @@ std::map<std::string, double> Dune::Multiscale::ErrorCalculator::print(std::ostr
     }
   }
 
+  const string msfem_exact = "msfem_exact", fem_exact = "fem_exact", msfem_fem = "msfem_fem";
+  unordered_map<string, DifferenceType> differences;
+  unordered_map<string, DiscreteDifferenceType> discrete_differences;
+  unordered_map<string, L2ErrorAnalytical> l2_analytical_errors;
+  unordered_map<string, H1sErrorAnalytical> h1s_analytical_errors;
+  unordered_map<string, L2ErrorDiscrete> l2_discrete_errors;
+  unordered_map<string, H1sErrorDiscrete> h1s_discrete_errors;
+  std::unique_ptr<DiscreteL2> l2_msfem;
+  constexpr auto pcw = std::piecewise_construct_t();
+
   //! ----------------- compute L2- and H1- errors -------------------
   if (Problem::getModelData()->hasExactSolution()) {
     const auto& u = *DMP::getExactSolution();
 
     if (msfem_solution_) {
-      const DifferenceType difference(u, fine_msfem_solution);
-      L2Localizable<CommonTraits::GridViewType, DifferenceType> l2_error_product(*grid_view, difference,
-                                                                                 over_integrate);
-      system_assembler.add(l2_error_product);
-      H1SemiLocalizable<CommonTraits::GridViewType, DifferenceType> h1_semi_error_product(*grid_view, difference,
-                                                                                          over_integrate);
-      system_assembler.add(h1_semi_error_product);
-      system_assembler.tbb_assemble();
-
-      const auto msfem_error = std::sqrt(l2_error_product.apply2());
-      out << "|| u_msfem - u_exact ||_L2 =  " << msfem_error << std::endl;
-      const auto h1_msfem_error = std::sqrt(h1_semi_error_product.apply2());
-      out << "|| u_msfem - u_exact ||_H1s =  " << h1_msfem_error << std::endl << std::endl;
-
-      csv["msfem_exact_L2"] = msfem_error;
-      csv["msfem_exact_H1s"] = h1_msfem_error;
+      const auto name = forward_as_tuple(msfem_exact);
+      const auto& difference = differences.emplace(pcw, name, forward_as_tuple(u, fine_msfem_solution)).first->second;
+      const auto product_args = forward_as_tuple(*grid_view, difference, over_integrate);
+      system_assembler.add(l2_analytical_errors.emplace(pcw, name, product_args).first->second);
+      system_assembler.add(h1s_analytical_errors.emplace(pcw, name, product_args).first->second);
     }
 
     if (fem_solution_) {
-      const DifferenceType difference(u, *fem_solution_);
-      L2Localizable<CommonTraits::GridViewType, DifferenceType> l2_error_product(*grid_view, difference,
-                                                                                 over_integrate);
-      system_assembler.add(l2_error_product);
-      H1SemiLocalizable<CommonTraits::GridViewType, DifferenceType> h1_semi_error_product(*grid_view, difference,
-                                                                                          over_integrate);
-      system_assembler.add(h1_semi_error_product);
-      system_assembler.tbb_assemble();
-
-      const auto fem_error = std::sqrt(l2_error_product.apply2());
-      out << "|| u_fem - u_exact ||_L2 =  " << fem_error << std::endl;
-      const auto h1_fem_error = std::sqrt(h1_semi_error_product.apply2());
-      out << "|| u_fem - u_exact ||_H1s =  " << h1_fem_error << std::endl << std::endl;
-
-      csv["fem_exact_L2"] = fem_error;
-      csv["fem_exact_H1s"] = h1_fem_error;
+      const auto name = forward_as_tuple(fem_exact);
+      const auto& difference = differences.emplace(pcw, name, forward_as_tuple(u, *fem_solution_)).first->second;
+      const auto product_args = forward_as_tuple(*grid_view, difference, over_integrate);
+      system_assembler.add(l2_analytical_errors.emplace(pcw, name, product_args).first->second);
+      system_assembler.add(h1s_analytical_errors.emplace(pcw, name, product_args).first->second);
     }
   }
 
   if (msfem_solution_ && fem_solution_) {
+    const auto name = forward_as_tuple(msfem_fem);
+    const auto& difference = discrete_differences.emplace(pcw, name, forward_as_tuple(fine_msfem_solution, *fem_solution_)).first->second;
+    const auto product_args = forward_as_tuple(*grid_view, difference, over_integrate);
+    system_assembler.add(l2_discrete_errors.emplace(pcw, name, product_args).first->second);
+    system_assembler.add(h1s_discrete_errors.emplace(pcw, name, product_args).first->second);
+    l2_msfem = DSC::make_unique<DiscreteL2>(*grid_view, fine_msfem_solution, over_integrate);
+    system_assembler.add(*l2_msfem);
+  }
 
-    typedef Stuff::Functions::Difference<CommonTraits::ConstDiscreteFunctionType,
-                                         CommonTraits::ConstDiscreteFunctionType> DiscreteDifferenceType;
-    const DiscreteDifferenceType difference(fine_msfem_solution, *fem_solution_);
+  system_assembler.tbb_assemble();
 
-    L2Localizable<CommonTraits::GridViewType, DiscreteDifferenceType> l2_error_product(*grid_view, difference,
-                                                                                       over_integrate);
-    system_assembler.add(l2_error_product);
-    L2Localizable<CommonTraits::GridViewType, CommonTraits::ConstDiscreteFunctionType> l2_msfem(
-        *grid_view, fine_msfem_solution, over_integrate);
-    system_assembler.add(l2_msfem);
-    H1SemiLocalizable<CommonTraits::GridViewType, DiscreteDifferenceType> h1_semi_error_product(*grid_view, difference,
-                                                                                                over_integrate);
-    system_assembler.add(h1_semi_error_product);
-    system_assembler.tbb_assemble();
+  if (Problem::getModelData()->hasExactSolution()) {
+    if(msfem_solution_) {
+      const auto msfem_error = std::sqrt(l2_analytical_errors.at(msfem_exact).apply2());
+      out << "|| u_msfem - u_exact ||_L2 =  " << msfem_error << std::endl;
+      const auto h1_msfem_error = std::sqrt(h1s_analytical_errors.at(msfem_exact).apply2());
+      out << "|| u_msfem - u_exact ||_H1s =  " << h1_msfem_error << std::endl << std::endl;
 
-    const auto approx_msfem_error = std::sqrt(l2_error_product.apply2());
-    const auto no = std::sqrt(l2_msfem.apply2());
+      csv[msfem_exact + "_L2"] = msfem_error;
+      csv[msfem_exact + "H1s"] = h1_msfem_error;
+    }
+
+    if (fem_solution_) {
+      const auto fem_error = std::sqrt(l2_analytical_errors.at(fem_exact).apply2());
+      out << "|| u_fem - u_exact ||_L2 =  " << fem_error << std::endl;
+      const auto h1_fem_error = std::sqrt(h1s_analytical_errors.at(fem_exact).apply2());
+      out << "|| u_fem - u_exact ||_H1s =  " << h1_fem_error << std::endl << std::endl;
+
+      csv[fem_exact + "_L2"] = fem_error;
+      csv[fem_exact + "_H1s"] = h1_fem_error;
+    }
+  }
+
+  if (msfem_solution_ && fem_solution_) {
+    const auto approx_msfem_error = std::sqrt(l2_discrete_errors.at(msfem_fem).apply2());
+    const auto no = std::sqrt(l2_msfem->apply2());
     if (std::abs(no) > 1e-12)
       out << "|| u_msfem - u_fem ||_L2 / || u_msfem ||_L2 =  " << approx_msfem_error / no << std::endl;
     else
       out << "|| u_msfem - u_fem ||_L2 =  " << approx_msfem_error << std::endl;
 
-    const auto h1_approx_msfem_error = std::sqrt(h1_semi_error_product.apply2());
+    const auto h1_approx_msfem_error = std::sqrt(h1s_discrete_errors.at(msfem_fem).apply2());
     out << "|| u_msfem - u_fem ||_H1s =  " << h1_approx_msfem_error << std::endl << std::endl;
 
-    csv["msfem_fem_L2"] = approx_msfem_error;
-    csv["msfem_fem_H1s"] = h1_approx_msfem_error;
+    csv[msfem_fem + "_L2"] = approx_msfem_error;
+    csv[msfem_fem + "_H1s"] = h1_approx_msfem_error;
   }
 
   std::unique_ptr<boost::filesystem::ofstream> csvfile(
@@ -182,6 +196,5 @@ std::map<std::string, double> Dune::Multiscale::ErrorCalculator::print(std::ostr
     *csvfile << key_val.second << sep;
   }
   *csvfile << std::endl;
-
   return csv;
 }
