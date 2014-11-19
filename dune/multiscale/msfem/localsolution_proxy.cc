5898215804feb380ac4f305a4ed01c0aaf1a62c9
Rene Milk <rene.milk@uni-muenster.de>
2014-11-19 15:18:50 +0100

    makes coarse scale prolong to local corrections explicit

--- dune/multiscale/msfem/localsolution_proxy.cc
+++ dune/multiscale/msfem/localsolution_proxy.cc
@@ -5,6 +5,7 @@
 #include <dune/multiscale/msfem/localproblems/localgridsearch.hh>
 #include <dune/multiscale/msfem/localsolution_proxy.hh>
 #include <dune/multiscale/msfem/proxygridview.hh>
+#include <dune/gdt/operators/prolongations.hh>
 
 Dune::Multiscale::LocalsolutionProxy::LocalsolutionProxy(CorrectionsMapType&& corrections,
                                                          const CommonTraits::SpaceType& coarseSpace,
@@ -24,6 +25,23 @@ Dune::Multiscale::LocalsolutionProxy::local_function(const BaseType::EntityType&
   if (it != corrections_.end())
     return it->second->local_function(entity);
   DUNE_THROW(InvalidStateException, "Coarse cell was not found!");
+}
+
+void Dune::Multiscale::LocalsolutionProxy::add(const Dune::Multiscale::CommonTraits::DiscreteFunctionType &coarse_func)
+{
+  const auto& coarse_space = coarse_func.space();
+  CorrectionsMapType targets;
+  for (auto& cr : corrections_) {
+    targets[cr.first] = DSC::make_unique<MsFEMTraits::LocalGridDiscreteFunctionType>(cr.second->space(), "tmpcorrection");
+  }
+  for (auto& range_pr : targets) {
+    auto id = range_pr.first;
+    auto& range = *range_pr.second;
+    const Dune::GDT::Operators::LagrangeProlongation<MsFEMTraits::LocalGridViewType> prolongation_operator(range.space().grid_view());
+    prolongation_operator.apply(coarse_func, range);
+    auto& correction = *corrections_[id];
+    correction.vector() += range.vector();
+  }
 }
 
 
