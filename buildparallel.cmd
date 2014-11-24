832b9808861fe52993d47046bcba7cd849bbe466
Rene Milk <rene.milk@uni-muenster.de>
2014-11-24 19:25:17 +0100

    parallel script stuff

--- buildparallel.cmd
+++ buildparallel.cmd
@@ -1,15 +1,15 @@
-#PBS -l walltime=00:06:00,nodes=1:ppn=12
+#PBS -l nice=-1,walltime=02:26:00,nodes=11:ppn=12
 #PBS -A o0num
-#PBS -M s_kaul01@uni-muenster.de
+#PBS -M r_milk01@wwu.de
 #PBS -m ae
 #PBS -q math
-#PBS -N build_parallel
+#PBS -N msfem
 #PBS -j oe
 #PBS -o output_build.txt
 
+# #PBS -t 32
+#PBS -o data/${PBS_JOBNAME%-*}/$PBS_JOBID/logdata/runlog.log
 cd $PBS_O_WORKDIR
-# source ../../local/src/tbb/build/linux_intel64_gcc_cc4.7.2_libc2.5_kernel2.6.18_debug/tbbvars.sh
-# export LD_LIBRARY_PATH=/Applic.PALMA/compiler/gcc/4.7.2/lib64:$LD_LIBRARY_PATH
-# source ../../BOOST_PATH.sh
-make -j 12 elliptic_msfem
-# /scratch/tmp/s_kaul01/external/bin/hpcstruct ./elliptic_msfem
+mkdir -p data/${PBS_JOBNAME%-*}/$PBS_JOBID/logdata/
+
+MPIHOME=/Applic.PALMA/mpi/openmpi/gcc4.7.2/1.6.3 likwid-mpirun  -d -mpi=openmpi -np $PBS_ARRAYID  -nperdomain N:12 -- "./elliptic_msfem ../parameter_files/msfem.param -global.datadir data/${PBS_JOBNAME%-*}/$PBS_JOBID"
--- submitrange.sh
+++ submitrange.sh
@@ -4,6 +4,6 @@ echo "Logging-level richtig eingestellt?"
 for (( c=$1; c<=$2; c*=2 ))
 do
     numnodes=$((($c+12-1)/12))
-    qsub -l nice=-18,walltime=01:00:00,nodes=$numnodes:westmere:ppn=12 -t $c ../test_scaleup.cmd
+    qsub -l nice=-18,walltime=03:00:00,nodes=$numnodes:westmere:ppn=12 -t $c ./speedup
 
 done
