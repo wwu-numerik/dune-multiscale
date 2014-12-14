bab426c8688e0161129ac12e36b0004e8cfba393
Rene Milk <rene.milk@uni-muenster.de>
2014-12-14 19:21:02 +0100

    [cmake] move flags around

--- cmake/modules/DuneMultiscaleMacros.cmake
+++ cmake/modules/DuneMultiscaleMacros.cmake
@@ -1,12 +1,16 @@
 message(AUTHOR_WARNING "TODO: Implement module test.")
 
-find_package(PETSc COMPONENTS CXX )
+include(DuneUtils)
+include(DuneMPI)
 
 find_package(METIS)
-find_package(ParMETIS REQUIRED)
+find_package(ParMETIS )
 include(cmake/modules/AddMETISFlags.cmake)
 include(AddParMETISFlags)
 include(CheckEmplace)
 
 find_package(Eigen3 3.2.0 REQUIRED)
 find_package(TBB REQUIRED)
+
+find_package(UMFPack REQUIRED)
+include(AddUMFPackFlags)
