c63d1a1cdcbfd440fcedbdd40ca3e02cc577e915
Rene Milk <rene.milk@uni-muenster.de>
2015-01-15 14:30:30 +0100

    correctly puts couple of scripts into cluster

--- submitrange.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Logging-level richtig eingestellt?"
-
-for (( c=$1; c<=$2; c*=2 ))
-do
-    numnodes=$((($c+12-1)/12))
-    qsub -l nice=-18,walltime=03:00:00,nodes=$numnodes:westmere:ppn=12 -t $c ./speedup
-
-done
