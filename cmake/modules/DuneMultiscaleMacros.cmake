0c2ebdad32d438772842388fd8978a745812f0ad
Rene Milk <rene.milk@uni-muenster.de>
2015-02-27 09:35:04 +0100

    [cmake] fixes using multiscale as a dependency in dune-mlmc

--- cmake/modules/DuneMultiscaleMacros.cmake
+++ cmake/modules/DuneMultiscaleMacros.cmake
@@ -5,7 +5,7 @@ include(DuneMPI)
 
 find_package(METIS)
 find_package(ParMETIS )
-include(cmake/modules/AddMETISFlags.cmake)
+include(AddMETISFlags)
 include(AddParMETISFlags)
 include(CheckEmplace)
 
@@ -17,4 +17,14 @@ include(AddUMFPackFlags)
 
 find_package(FFTW REQUIRED)
 include_directories(${FFTW_INCLUDES})
-message(STATUS ${FFTW_LIBRARIES})
+
+set(GRIDDIM 3 CACHE STRING "")
+set(USE_ISTL_BACKEND 1 CACHE BOOLEAN "use dune-istl as the la-backend. Disable to use Eigen3 instead.")
+set(USE_FEM_BACKEND 0 CACHE BOOLEAN "use dune-fem as the discretization-backend. Disable to use dune-pdelab instead.")
+
+If    ("${CMAKE_BUILD_TYPE}" MATCHES "^REL")
+  ADD_IF_SUPPORTED(CMAKE_CXX_FLAGS_RELEASE "-funroll-loops" "-m64" "-mfpmath=sse" "-falign-loops" "-mtune=native" "-march=native" 
+  "-pipe" "-fomit-frame-pointer" "-O4" "-fno-alias" )
+EndIf ("${CMAKE_BUILD_TYPE}" MATCHES "^REL")
+
+add_definitions("-DSPGRID" "-DENABLE_PARMETIS=1" "-DGRIDDIM=${GRIDDIM}" -DMETISNAMEL )
