6903e8ed13573c0cfea7f02b171189dd476eaf2c
Rene Milk <rene.milk@uni-muenster.de>
2015-03-11 14:26:02 +0100

    [cmake] fixes optional fftw linkage

--- cmake/modules/DuneMultiscaleMacros.cmake
+++ cmake/modules/DuneMultiscaleMacros.cmake
@@ -21,7 +21,7 @@ find_package(FFTW )
 if(FFTW_FOUND)
 	include_directories(${FFTW_INCLUDES})
 	set(HAVE_RANDOM_PROBLEM 1)
-	set(COMMON_LIBS fftw3_mpi ${FFTW_LIBRARIES})
+	set(COMMON_LIBS ${COMMON_LIBS} fftw3_mpi ${FFTW_LIBRARIES})
 endif(FFTW_FOUND)
 
 set(GRIDDIM 3 CACHE STRING "")
