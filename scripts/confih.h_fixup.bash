9a51132ada4943d5fbf2f6e56cbd0b6d8e31c97c
Rene Milk <rene.milk@uni-muenster.de>
2015-03-27 10:23:22 +0100

    [scripts] moved to seperate repo

--- scripts/confih.h_fixup.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd $(dirname ${0})/..
-# remove all config.h includes
-find . -name "*.hh" | xargs sed -i "s;#include\ \<config\.h\>;;g"
-find . -name "*.cc" | xargs sed -i "s;#include\ \<config\.h\>;;g"
-# insert as 1st line
-find . -name "*.cc" | xargs sed -i "1i #include\ <config\.h>"
-
