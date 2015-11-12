3eaca938796308d9299675902a263de820b7c928
Rene Milk <rene.milk@wwu.de>
2015-11-12 20:28:39 +0100

    minimal -> thread test

--- src/minimal.cc
+++ src/minimal.cc
@@ -7,42 +7,69 @@
 #include <dune/multiscale/fem/algorithm.hh>
 #include <dune/stuff/common/parallel/helper.hh>
 #include <dune/stuff/common/profiler.hh>
+#include <dune/stuff/common/parallel/threadmanager.hh>
 #include <thread>
 
+#include <tbb/blocked_range.h>
+#include <vector>
 #include <tbb/task_scheduler_init.h>
+using namespace std;
+using namespace tbb;
+
+class NumberPrinter {
+private:
+    static void develop(size_t i);
+
+public:
+
+    void operator()(const blocked_range<size_t>& r);
+    NumberPrinter(NumberPrinter& x, split);
+    NumberPrinter(int highbit);
+
+    void join(const NumberPrinter& y);
+
+};
+void NumberPrinter::develop(size_t i)
+{
+  const auto mm = DS::threadManager().thread();
+  cout << "Thread " << mm << ": " << i << std::endl;
+
+}
+
+
+void NumberPrinter::operator()( const blocked_range<size_t>& r) {
+    for(size_t i=r.begin(); i!=r.end(); ++i)
+        develop(i);
+}
+
+NumberPrinter::NumberPrinter( NumberPrinter& x, split) {}
+NumberPrinter::NumberPrinter(int highbit) {}
+
+void NumberPrinter::join(const NumberPrinter& ) {
+}
+
+void printNumbers (int n) {
+    NumberPrinter p(n);
+    parallel_reduce(blocked_range<size_t>(0,n), p, auto_partitioner());
+}
 
 int main(int argc, char** argv) {
   using namespace Dune::Multiscale;
   try {
-#if DUNE_MULTISCALE_WITH_DUNE_FEM
-//  Dune::Fem::MPIManager::initialize(argc, argv);
-#endif
-  auto& helper = Dune::MPIHelper::instance(argc, argv);
-//  if (helper.size() > 1 && !(Dune::Capabilities::isParallel<Dune::Multiscale::CommonTraits::GridType>::v)) {
-//    DUNE_THROW(Dune::InvalidStateException, "mpi enabled + serial grid = bad idea");
-//  }
-//  DSC::Config().read_command_line(argc, argv);
-//  DSC::testCreateDirectory(DSC_CONFIG_GET("global.datadir", "data/"));
+    init(argc, argv);
 
-//  // LOG_NONE = 1, LOG_ERROR = 2, LOG_INFO = 4,LOG_DEBUG = 8,LOG_CONSOLE = 16,LOG_FILE = 32
-//  // --> LOG_ERROR | LOG_INFO | LOG_DEBUG | LOG_CONSOLE | LOG_FILE = 62
-//  DSC::Logger().create(DSC_CONFIG_GET("logging.level", 62),
-//                       DSC_CONFIG_GET("logging.file", std::string(argv[0]) + ".log"),
-//                       DSC_CONFIG_GET("global.datadir", "data"),
-//                       DSC_CONFIG_GET("logging.dir", "log" /*path below datadir*/));
-//  DSC_PROFILER.setOutputdir(DSC_CONFIG_GET("global.datadir", "data"));
-//  DS::threadManager().set_max_threads(DSC_CONFIG_GET("threading.max_count",1));
+    //!TODO include base in config
+    DSC_PROFILER.startTiming("msfem.all");
 
-//    DSC_PROFILER.startTiming("total_cpu");
+    printNumbers(10000);
 
-//    cgfem_algorithm();
 
-//    const auto cpu_time =
-//        DSC_PROFILER.stopTiming("total_cpu") / 1000.f;
-//    MS_LOG_INFO_0 << "Total runtime of the program: " << cpu_time << "s" << std::endl;
-//    DSC_PROFILER.outputTimings("profiler");
-//    mem_usage();
-//    dump_environment();
+    const auto cpu_time =
+        DSC_PROFILER.stopTiming("total_cpu") / 1000.f;
+    MS_LOG_INFO_0 << "Total runtime of the program: " << cpu_time << "s" << std::endl;
+    DSC_PROFILER.outputTimings("profiler");
+    mem_usage();
+    dump_environment();
   } catch (Dune::Exception& e) {
     return handle_exception(e);
   } catch (std::exception& s) {
