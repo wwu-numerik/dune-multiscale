8e01a46df713a57d64fa0ef8f01f94233966fd97
Rene Milk <rene.milk@wwu.de>
2016-04-13 11:05:01 +0200

    [cmake] transition RANDOM_PROBLEM -> FFTW, update lib order

--- cmake/modules/DuneMultiscaleMacros.cmake
+++ cmake/modules/DuneMultiscaleMacros.cmake
@@ -9,8 +9,6 @@ include(AddMETISFlags)
 include(AddParMETISFlags)
 include(CheckEmplace)
 
-find_package(Eigen3 3.2.0)
-
 find_package(UMFPack)
 find_package(SuiteSparse)
 include_directories( ${SUITESPARSE_INCLUDE_DIRS} )
@@ -30,10 +28,5 @@ set( MULTISCALE_LIBS multiscale_common multiscale_cgfem multiscale_msfem multisc
 set(GRIDDIM 3 CACHE STRING "")
 set(USE_ISTL_BACKEND 1 CACHE BOOLEAN "use dune-istl as the la-backend. Disable to use Eigen3 instead.")
 set(USE_FEM_BACKEND 0 CACHE BOOLEAN "use dune-fem as the discretization-backend. Disable to use dune-pdelab instead.")
-
-If    ("${CMAKE_BUILD_TYPE}" MATCHES "^REL")
-  ADD_IF_SUPPORTED(CMAKE_CXX_FLAGS_RELEASE "-funroll-loops" "-m64" "-mfpmath=sse" "-falign-loops" "-mtune=native" "-march=native" 
-  "-pipe" "-fomit-frame-pointer" "-O4" "-fno-alias" )
-EndIf ("${CMAKE_BUILD_TYPE}" MATCHES "^REL")
 
 add_definitions("-DENABLE_PARMETIS=1" "-DGRIDDIM=${GRIDDIM}" "-DMETISNAMEL" )
