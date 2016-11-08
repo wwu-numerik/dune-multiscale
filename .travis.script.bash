d9f1fa2385c4694faec93dcdebd91dbaac880b2e
Rene Milk <rene.milk@wwu.de>
2016-11-08 13:04:32 +0100

    [travis] use docker

---
+++ .travis.script.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+${SRC_DCTRL} ${BLD} --only=${MY_MODULE} configure
+if [[ "x${TESTS}" != "xheadercheck" ]]; then
+    ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make
+    ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v -j 1 test_binaries
+    ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja check
+    #${SUPERDIR}/.travis/init_sshkey.sh ${encrypted_95fb78800815_key} ${encrypted_95fb78800815_iv} keys/dune-community/dune-gdt-testlogs
+    #${SUPERDIR}/scripts/bash/travis_upload_test_logs.bash ${SUPERDIR}/${MY_MODULE}/${DUNE_BUILD_DIR}/dune/multiscale/test/
+
+else
+    ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja headercheck
+fi
+
+unset GH_TOKEN
+${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make install | grep -v "Installing"
+${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make package_source
+
