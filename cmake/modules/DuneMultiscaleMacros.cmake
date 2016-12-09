8f740469e647706844f3f62ea0ced023b3eb15c3
Rene Milk <rene.milk@wwu.de>
2016-12-09 15:02:47 +0100

    make parmetis optional for docker

--- cmake/modules/DuneMultiscaleMacros.cmake
+++ cmake/modules/DuneMultiscaleMacros.cmake
@@ -2,10 +2,6 @@ message(AUTHOR_WARNING "TODO: Implement module test.")
 
 find_package(PETSc COMPONENTS CXX )
 
-find_package(METIS)
-find_package(ParMETIS REQUIRED)
-include(cmake/modules/AddMETISFlags.cmake)
-include(AddParMETISFlags)
 include(CheckEmplace)
 
 find_package(Eigen3 3.2.0 REQUIRED)
